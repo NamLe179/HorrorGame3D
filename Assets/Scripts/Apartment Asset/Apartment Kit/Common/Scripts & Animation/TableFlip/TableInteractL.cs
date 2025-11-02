648e8877fe422e196a1cccce8a4596fc3d611428
Tai Phan <taip7547@gmail.com>
2025-11-02 19:38:16 +0700

    Luu Y Khi Merge

---
+++ Assets/Scripts/Apartment Asset/Apartment Kit/Common/Scripts & Animation/TableFlip/TableInteractL.cs
@@ -0,0 +1,56 @@
+﻿//using System.Collections;
+//using UnityEngine;
+
+//namespace HorrorGame3D.Interaction
+//{
+//	public class TableInteractL : InteractableBase
+//	{
+//		public Animator animator;
+
+//		void Start()
+//		{
+//			if (animator == null)
+//			{
+//				animator = GetComponent<Animator>();
+//			}
+//		}
+
+//		public override void Interact(Transform player)
+//		{
+//			if (!CanInteract())
+//			{
+//				return;
+//			}
+
+//			if (isOpen)
+//				StartCoroutine(CloseDoor());
+//			else
+//				StartCoroutine(OpenDoor());
+//		}
+
+//		public override string GetPromptMessage()
+//		{
+//			if (isLocked)
+//			{
+//				if (requiredKey == null) return "🔒 Locked. Need a key.";
+//				else return $"🔒 Locked. Need a {requiredKey.displayName}";
+//			}
+
+//			return isOpen ? "Press E to close" : "Press E to open";
+//		}
+
+//		private IEnumerator OpenDoor()
+//		{
+//			animator.Play("Lup");
+//			isOpen = true;
+//			yield return new WaitForSeconds(0.5f);
+//		}
+
+//		private IEnumerator CloseDoor()
+//		{
+//			animator.Play("Ldown");
+//			isOpen = false;
+//			yield return new WaitForSeconds(0.5f);
+//		}
+//	}
+//}
